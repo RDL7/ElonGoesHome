5f343acc0cc625b7a1a4efc0cfadeb94079737d9
Roberts Ivanvos <robertsivanovs1999@gmail.com>
2020-03-07 10:20:48 +0200

    Can switch between cameras but no zooming happens

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -6,11 +6,14 @@ public class CameraSwitch : MonoBehaviour
 {
     public GameObject toZoomInto;
     public GameObject cameraOne;
+    private bool cameraOneZooming;
+    private bool cameraOneZoomedIn;
+
     public GameObject cameraTwo;
+    private bool cameraTwoZooming;
+    private bool cameraTwoZoomedIn;
     private double minZoom = 0.25f;
     private double maxZoom = 5f;
-    private bool zoomedIn = false;
-    private bool currentlyZoomingIn = false;
     private GameObject activeCamera;
     void Start()
     {
@@ -26,44 +29,53 @@ public class CameraSwitch : MonoBehaviour
             this.cameraOne.gameObject.SetActive(false);
             this.cameraTwo.gameObject.SetActive(true);
             this.activeCamera = this.cameraTwo;
+            this.cameraOneZoomedIn = false;
+            this.cameraOneZooming = true;
+            this.cameraTwoZoomedIn = true;
+            this.cameraTwoZooming = false;
         }
         else
         {
             this.cameraOne.gameObject.SetActive(true);
             this.cameraTwo.gameObject.SetActive(false);
             this.activeCamera = this.cameraOne;
+            this.cameraTwoZoomedIn = false;
+            this.cameraTwoZooming = true;
+            this.cameraOneZoomedIn = true;
+            this.cameraOneZooming = false;
         }
     }
 
     void Update()
     {
-        double target = this.zoomedIn ? this.minZoom : this.maxZoom;
-        Vector3 targetPosition = this.zoomedIn ? toZoomInto.transform.position : new Vector3(0, 0, -10);
-
-        this.updateZoom(target);
-        this.updatePosition(targetPosition);
-        if (this.activeCamera.GetComponent<Camera>().orthographicSize == target)
-        {
-            this.currentlyZoomingIn = false;
+        bool innerZoomedIn;
+        bool innerZoomingIn;
+        if (this.activeCamera == this.cameraOne) {
+            innerZoomedIn = this.cameraOneZoomedIn;
+            innerZoomingIn = this.cameraOneZooming;
+        } else {
+            innerZoomedIn = this.cameraTwoZoomedIn;
+            innerZoomingIn = this.cameraTwoZooming;
         }
+        double target = innerZoomingIn ? this.minZoom : this.maxZoom;
+        Vector3 targetPosition = innerZoomingIn ? toZoomInto.transform.position : new Vector3(0, 0, -10);
+
+        this.updateZoom(target, innerZoomedIn, innerZoomingIn);
+        this.updatePosition(targetPosition, innerZoomedIn, innerZoomingIn);
 
         if (Input.GetKeyDown("e"))
         {
-            this.currentlyZoomingIn = true;
-            this.activeCamera
-                .GetComponent<Camera>()
-                .orthographicSize = (float)(this.zoomedIn ? this.minZoom : this.maxZoom);
-            this.zoomedIn = !this.zoomedIn;
+            this.toggleCamera();
         }
     }
 
-    void updateZoom(double target)
+    void updateZoom(double target, bool zoomedIn, bool zoomingIn)
     {
-        double step = this.zoomedIn ? -0.25 : 0.25;
+        double step = zoomedIn ? -0.25 : 0.25;
         double current = this.activeCamera
             .GetComponent<Camera>()
             .orthographicSize;
-        if (this.currentlyZoomingIn)
+        if (zoomingIn)
         {
             current += step;
             this.activeCamera
@@ -72,18 +84,17 @@ public class CameraSwitch : MonoBehaviour
         }
     }
 
-    void updatePosition(Vector3 target)
+    void updatePosition(Vector3 target, bool zoomedIn, bool zoomingIn)
     {
-        if (this.currentlyZoomingIn)
+        if (zoomedIn)
         {
             Vector3 newLocation = Vector3.MoveTowards(this.transform.position, target, (float)1);
             this.transform.position = new Vector3(newLocation.x, newLocation.y, -10);
         }
-        else if (this.zoomedIn)
+        else if (zoomingIn)
         {
             this.transform.position = new Vector3(target.x, target.y, -10);
             this.toggleCamera();
-            // this.zoomedIn = false;
         }
     }
 }
