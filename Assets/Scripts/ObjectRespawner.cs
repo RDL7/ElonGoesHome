e95c0f32d4df7d0cc6edd4789b2ba57cbbd433ab
Roberts Ivanvos <robertsivanovs1999@gmail.com>
2020-03-07 17:22:53 +0200

    spawned objects are no longer as shit as they were

--- Assets/Scripts/ObjectRespawner.cs
+++ Assets/Scripts/ObjectRespawner.cs
@@ -19,16 +19,17 @@ public class ObjectRespawner : MonoBehaviour
         {
             Vector3 pos = new Vector3(UnityEngine.Random.Range(-8, 8), this.transform.position.y, this.transform.position.z);
             GameObject gm = Instantiate(this.asteroidPrefab, pos, Quaternion.identity) as GameObject;
-            gm.GetComponent<Rigidbody2D>().mass = UnityEngine.Random.Range(1, 3);
-            gm.GetComponent<Rigidbody2D>().gravityScale = gm.GetComponent<Rigidbody2D>().mass;
-            gm.gameObject.transform.localScale = new Vector3(UnityEngine.Random.Range(1, 4), UnityEngine.Random.Range(1, 4), 0);
+            float rand = UnityEngine.Random.Range(0.5f, 1.5f);
+            gm.GetComponent<Rigidbody2D>().mass = 1;
+            gm.GetComponent<Rigidbody2D>().gravityScale = rand;
+            gm.gameObject.transform.localScale = new Vector3(rand, rand, 0);
         }
     }
     IEnumerator asteroidWave()
     {
         while (true)
         {
-            yield return new WaitForSeconds(UnityEngine.Random.Range(0, 2));
+            yield return new WaitForSeconds(UnityEngine.Random.Range(0f, 1f));
             spawnEnemy();
         }
     }
