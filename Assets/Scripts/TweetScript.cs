fcf205bed5bebd4312dc158484e2b2ca21b29fdc
Roberts Ivanvos <robertsivanovs1999@gmail.com>
2020-03-07 21:58:23 +0200

    Added tweets to the game

---
+++ Assets/Scripts/TweetScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweetScript : MonoBehaviour
+{
+
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 1 * Time.deltaTime);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/TweetSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TweetSpawner : MonoBehaviour
+{
+    public GameObject TweetPrefab;
+
+    void Start() {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter() {
+        this.spawn("Use 'WASD' or Arrow keys to move around");
+        yield return new WaitForSeconds(3f);
+        this.spawn("'Space' to shoot");
+        yield return new WaitForSeconds(3f);
+        this.spawn("'E' to check out Elon");
+    }
+
+    public void spawn(string text) {
+        GameObject a = Instantiate(this.TweetPrefab, this.gameObject.transform.position, Quaternion.identity, this.gameObject.transform) as GameObject;
+        a.transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>().text = text.ToString();
+    }
+}
