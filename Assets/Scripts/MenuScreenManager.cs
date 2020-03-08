867a350b2250234b345b50ba14aadc25f9acb559
Roberts Ivanvos <robertsivanovs1999@gmail.com>
2020-03-08 13:49:06 +0200

    Created menu screen scene

---
+++ Assets/Scripts/MenuScreenManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScreenManager : MonoBehaviour
+{
+    public void ExitGame() {
+        print("QUIT");
+        Application.Quit();
+    }
+    public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
